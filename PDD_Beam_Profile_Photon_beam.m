457f927621c71ae371d8665053b1d1901858daaa
Megha Rai <87697964+megharai40@users.noreply.github.com>
2025-01-18 21:18:41 +0530

    Update PDD_Beam_Profile_Photon_beam.m

--- PDD_Beam_Profile_Photon_beam.m
+++ PDD_Beam_Profile_Photon_beam.m
@@ -2,7 +2,8 @@
 clc
 clear
 clear all
-T = readtable('New Microsoft Excel Worksheet (4)', 'Sheet', '6X');
+%The first argument of readtable is the path to the excel file with raw beam data, the second and third argument specify the sheet name in the excel file to be analysed
+T = readtable('RAW DATA EXCEL SHEET', 'Sheet', '6X'); 
 A = table2array(T);
 
 %defining depth and dose columns for PDD
